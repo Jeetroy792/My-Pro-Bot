e9abe66b2e1937e81c3c4b5580eed48e9ef9f48a
Jeetroy792 <jeetroy5335@gmail.com>
2026-02-08 13:44:24 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from config import Config
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="ForwarderPro",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            plugins=dict(root="plugins")
+        )
+      
