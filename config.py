b6d4e88c50a4fd6010af841cb937b9d2ee2e7d85
Jeetroy792 <jeetroy5335@gmail.com>
2026-02-08 15:58:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
-class Config:
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+class Config(object):
+    # Koyeb থেকে ভেরিয়েবলগুলো রিড করার সঠিক পদ্ধতি
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH", "")
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-    SHORTENER_URL = os.environ.get("SHORTENER_URL", "shareus.io")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 8229228616)) # আপনার আইডি ডিফল্ট হিসেবে থাকল
     SHORTENER_API = os.environ.get("SHORTENER_API", "")
-  
+    SHORTENER_URL = os.environ.get("SHORTENER_URL", "gplinks.in")
